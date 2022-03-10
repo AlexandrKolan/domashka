ace53f81e1b2244ee1c37aee8f1278c1afa54373
AlexandrKolan <shura.kolandariya@bk.ru>
2022-03-10 18:19:13 +0300

    1

--- project 0/ugadivanie.py
+++ project 0/ugadivanie.py
@@ -18,5 +18,19 @@ def random_predict(number: int = 1) -> int:
         else:
             print(f"Компьютер угадал загаданное число {predict_number} число за {count} попыток.")
             break
+    return count
    
 random_predict()
+def score_game(random_predict) -> int:
+    count_ls = []
+    np.random.seed(1) 
+    random_array = np.random.randint(1, 101, size=(1000)) 
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+score_game(random_predict)
